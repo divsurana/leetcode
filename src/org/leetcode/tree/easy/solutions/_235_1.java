5d4cc32e440b658a8afba158e8f5acbdff56efd0
Divyesh Surana <divyeshsurana@gmail.com>
2019-07-17 00:24:32 -0400

    Adding solutions to problems

---
+++ src/org/leetcode/tree/easy/solutions/_235_1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.leetcode.tree.easy.solutions;
+
+import org.leetcode.bst.medium.solutions.TreeNode;
+
+/**
+ * @author divyesh_surana
+ *
+ */
+public class _235_1 {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root.val > p.val && root.val > q.val)
+			return lowestCommonAncestor(root.left, p, q);
+		if (root.val < p.val && root.val < q.val)
+			return lowestCommonAncestor(root.right, p, q);
+		return root;
+	}
+}
