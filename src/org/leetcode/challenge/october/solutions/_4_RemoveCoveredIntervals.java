6a20fb7511e5e8ecb8bf667fce443fb98e88cfee
Divyesh Surana <divyeshsurana@gmail.com>
2020-10-04 23:29:43 -0400

    Update to solution 4 Oct

--- src/org/leetcode/challenge/october/solutions/_4_RemoveCoveredIntervals.java
+++ src/org/leetcode/challenge/october/solutions/_4_RemoveCoveredIntervals.java
@@ -13,13 +13,31 @@ import java.util.Comparator;
 public class _4_RemoveCoveredIntervals {
 	// Sort by smallest interval to largest and then check whether eahc interval is
 	// a part of the bigger intervals after itx
-	public int removeCoveredIntervals(int[][] intervals) {
+	public int removeCoveredIntervals_v1(int[][] intervals) {
 		Arrays.sort(intervals, Comparator.comparing(o -> o[1] - o[0]));
 		int result = intervals.length;
 
 		for (int i = 0; i < intervals.length - 1; i++) {
 			int[] interval = intervals[i];
 			for (int j = i + 1; j < intervals.length; j++) {
+				if (interval[0] >= intervals[j][0] && interval[1] <= intervals[j][1]) {
+					result--;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	// Checking intervals in reverse order is faster
+	public int removeCoveredIntervals_v2(int[][] intervals) {
+		Arrays.sort(intervals, Comparator.comparing(o -> o[1] - o[0]));
+		int result = intervals.length;
+
+		for (int i = 0; i < intervals.length - 1; i++) {
+			int[] interval = intervals[i];
+			for (int j = intervals.length - 1; j > i; j--) {
 				if (interval[0] >= intervals[j][0] && interval[1] <= intervals[j][1]) {
 					result--;
 					break;
